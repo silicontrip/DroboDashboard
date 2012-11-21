a45602e7447125d6c555c96164592f42f4318635
Mark Heath <mark@silicontrip.net>
2012-11-22 07:54:54 +1100

    Added print Selector code. To determine the Selectors of the DDServer member functions list.

--- Makefile
+++ Makefile
@@ -2,9 +2,14 @@
 COCOAFLAGS=-framework Foundation
 OBJ=ESATMUpdate.o dash.o
 
-all: dash
+all: dash printSelector
 
 ESATMUpdate.o: ESATMUpdate.h ESATMUpdate.m
 
+printSelector.o: printSelector.m 
+
 dash: $(OBJ)
 	$(CC) $(COCOAFLAGS) $(OBJ) -o dash
+
+printSelector: ESATMUpdate.o printSelector.o
+	$(CC) $(COCOAFLAGS) ESATMUpdate.o printSelector.o -o printSelector
---
+++ printSelector.m
@@ -0,0 +1,260 @@
+/*
+ ** Very basic drobo tool
+ ** communicates with DDServiced via Objective-C Distributed objects.
+ ** 
+ ** Methods for the DDServer Class found using strings on the DDServiced binary
+ ** 
+ gcc  -framework Foundation dash.m
+ */
+
+#import <Foundation/NSConnection.h>
+#import <Foundation/NSPortNameServer.h>
+#import <Foundation/Foundation.h>
+#import <Foundation/NSString.h>
+#import <Foundation/NSXMLElement.h>
+#import <objc/runtime.h>
+
+#import "DDServer.h"
+
+NSAutoreleasePool  *pool;
+NSDistantObject *proxy;
+
+char *typeDecoding (const char *code) {
+	
+	static char decode[1024];
+	int index;
+	
+	decode[0] = '\0';
+	
+	//	decode = [[String alloc] init];
+	
+	for (index=0; index < strlen(code); index++) {
+		
+		if (index > 0)
+			strcat(decode," ");
+		
+		switch (code[index]) {
+				
+			case 'c':
+				strcat(decode,"char");	
+				break;
+				
+			case 'i':
+				strcat(decode,"int");	
+				break;
+				
+			case 's':
+				strcat(decode,"short");	
+				break;
+				
+			case 'l':
+				strcat(decode,"long");	
+				break;
+				
+			case 'q':
+				strcat(decode,"longlong");	
+				break;
+				
+			case 'C':
+				strcat(decode,"unsignedchar");	
+				break;
+				
+			case 'I':
+				strcat(decode,"unsignedint");	
+				break;
+				
+			case 'S':
+				strcat(decode,"unsignedshort");	
+				break;
+			case 'L':
+				strcat(decode,"unsignedlong");	
+				break;
+			case 'Q':
+				strcat(decode,"unsignedlonglong");	
+				break;
+				
+			case 'f':
+				strcat(decode,"float");
+				break;
+				
+			case 'd':
+				strcat(decode,"double");	
+				break;
+				
+			case 'B':
+				strcat(decode,"bool");	
+				break;
+				
+			case 'v':
+				strcat(decode,"void");	
+				break;
+				
+			case '*':
+				strcat(decode,"char*");	
+				break;
+				
+			case '@':
+				strcat(decode,"*Object");	
+				break;
+			case '#':
+				strcat(decode,"Class");	
+				break;
+				
+			case ':':
+				strcat(decode,"Selector");	
+				break;
+				
+			case '[':
+				strcat(decode,"array[");	
+				break;
+			case ']':
+				strcat(decode,"]");	
+				break;
+				
+			case '{':
+				strcat(decode,"struct{");	
+				break;
+			case '}':
+				strcat(decode,"}");	
+				break;
+				
+			case '(':
+				strcat(decode,"union(");	
+				break;
+			case ')':
+				strcat(decode,")");	
+				break;
+				
+			case 'b':
+				strcat(decode,"bits");	
+				break;
+				
+			case '^':
+				strcat(decode,"*");	
+				break;
+				
+			case '?':
+				strcat(decode,"unknown");	
+				break;
+				
+			case 'r':
+				strcat(decode,"const");	
+				break;
+				
+			case 'n':
+				strcat(decode,"in");	
+				break;
+				
+			case 'N':
+				strcat(decode,"inout");	
+				break;
+				
+			case 'o':
+				strcat(decode,"out");	
+				break;
+				
+			case 'O':
+				strcat(decode,"bycopy");	
+				break;
+				
+			case 'R':
+				strcat(decode,"byref");	
+				break;
+				
+			case 'V':
+				strcat(decode,"oneway");	
+				break;
+				
+			default:
+				break;
+				
+		}
+	}	
+	
+	return decode;
+	
+}
+
+printSelectorString (NSString *selString) {
+	int index;
+	NSMethodSignature *sig;
+	
+	NSArray *select  = [selString componentsSeparatedByString:@":"];
+	
+	sig = [proxy methodSignatureForSelector:NSSelectorFromString(selString)];
+
+	printf ("- (%s)",typeDecoding([sig methodReturnType]));
+	
+	for (index=2; index < [sig numberOfArguments]; index++) {
+		printf ("%s:(%s) ",[[select objectAtIndex:index-2] UTF8String], typeDecoding([sig getArgumentTypeAtIndex:index])); 
+	}
+	printf("\n");
+}
+
+int main(int argc, char *argv[])
+{
+	
+	NSSocketPort *port;
+	NSConnection *connection;
+	
+	
+	DDServer *dd ;	
+	NSString *esaid;
+	NSString *esaupdate;
+	NSData *command;
+	
+	Method * mlist;
+	unsigned int mc = 0;
+	int index = 0;
+	int droboCount;
+	
+	int ddservicedPort = 50005;
+	
+	char rVal;
+	unsigned long ulVal;
+	
+	pool = [[NSAutoreleasePool alloc] init];
+	
+	port = [[NSSocketPort alloc] initRemoteWithTCPPort:ddservicedPort host:@"localhost"];
+	
+	if (port == nil) {
+		// this is actually a programming error
+		NSLog(@"could not setup port");
+		exit(-1);
+	}
+	
+	connection = [NSConnection connectionWithReceivePort:nil sendPort:port];
+	proxy = [[connection rootProxy] retain];
+	
+	dd = (DDServer *)proxy;
+
+	NSLog(@"%@",[proxy description]);
+	
+	if ([dd subscribeClient:proxy] == 1) {
+		
+		int riVal;
+		
+		NSString *fileContents = [NSString stringWithContentsOfFile:@"selector.txt" encoding:NSUTF8StringEncoding error:nil];
+		NSArray *lines = [fileContents componentsSeparatedByString:@"\n"];
+
+		for (NSString* o in lines)
+		{
+			if ([o length] > 0 ) {
+			printSelectorString(o);
+			}
+		}		
+		NSString *esaid = [[NSString alloc] init];
+		NSString *esaupdate = [[NSString alloc] init];
+		NSString *command = [[NSData alloc] init];
+		
+		if ([dd getESAId:proxy ESAAtIndex:0 ESAID:&esaid]>0) {
+		
+			[dd Identify:proxy  ESAID:esaid];
+			
+		}
+			
+	}	
+	
+	[pool release];
+	
+}
